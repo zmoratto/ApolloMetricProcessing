2fa1bc3be05b4bed4a7e127245e3111b9801be21
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-07-03 20:25:31 -0700

    Finished extract clementine GCP
    It works and puts it's output in a CNET.

--- tools/src/extract_clementine_gcp.cc
+++ tools/src/extract_clementine_gcp.cc
@@ -3,6 +3,9 @@
 #include <vw/FileIO.h>
 #include <vw/Camera.h>
 #include <vw/Cartography.h>
+#include <vw/InterestPoint.h>
+#include <vw/Math.h>
+#include <vw/BundleAdjustment/ControlNetwork.h>
 
 #include <boost/filesystem/path.hpp>
 #include <boost/program_options.hpp>
@@ -12,6 +15,8 @@ namespace fs = boost::filesystem;
 using namespace vw;
 using namespace vw::camera;
 using namespace vw::cartography;
+using namespace vw::ip;
+using namespace vw::ba;
 
 int main( int argc, char* argv[] ) {
   std::vector<std::string> input_file_names;
@@ -34,7 +39,7 @@ int main( int argc, char* argv[] ) {
   po::notify( vm );
 
   std::ostringstream usage;
-  usage << "Usage: " << argv[0] << "[options] <image-files> ... " << std::endl << std::endl;
+  usage << "Usage: " << argv[0] << "[options] <image-files> ...\n\n";
   usage << general_options << std::endl;
 
   if( vm.count("help") ) {
@@ -46,19 +51,21 @@ int main( int argc, char* argv[] ) {
     return 1;
   }
 
+  std::string lola_names("/Users/zack/Data/Moon/LOLA/LOLA_64px_p_deg_DEM.tif");
+
   std::vector<std::string> clementine_names;
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_30n045_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_30n135_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_30n225_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_30n315_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_30s045_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_30s135_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_30s225_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_30s315_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_75n090_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_75n270_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_75s090_256ppd.tif");
-  clementine_names.push_back("/Users/zmoratto/Data/Moon/Clementine/ClementineBaseMapV2/clembase_75s270_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_30n045_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_30n135_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_30n225_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_30n315_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_30s045_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_30s135_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_30s225_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_30s315_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_75n090_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_75n270_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_75s090_256ppd.tif");
+  clementine_names.push_back("/Users/zack/Data/Moon/Clementine/BaseMapV2/clembase_75s270_256ppd.tif");
 
   // Load up those files georeference information
   std::vector<GeoReference> clementine_georef;
@@ -76,37 +83,127 @@ int main( int argc, char* argv[] ) {
     PinholeModel pinhole( pinhole_name );
 
     Vector3 llr = XYZtoLonLatRadFunctor()( pinhole.camera_center() );
+    std::string match_filename;
 
     // Finding an image that actually contains this camera's location
     for ( unsigned i = 0; i < clementine_names.size(); i++ ) {
-      Vector2 clem_pix = clementine_georef[i].point_to_pixel(clementine_georef[i].lonlat_to_point(subvector(llr,0,2)));
+      Vector2 clem_pix = clementine_georef[i].lonlat_to_pixel(subvector(llr,0,2));
       if ( clementine_bbox[i].contains(clem_pix) ) {
         // Create a new GeoReference
         GeoReference georef_dest;
         georef_dest.set_well_known_geogcs("D_MOON");
         georef_dest.set_orthographic( llr[1], llr[0] );
         Matrix<double> itx = vw::math::identity_matrix<3>();
-        double zoom = 40;
+        double zoom = 160;
         itx(0,0) = zoom;
         itx(1,1) = -zoom;
         //itx(0,2) = itx(1,2) = 00*zoom;
-        itx(0,2) = -2000*zoom;
-        itx(1,2) = 2000*zoom;
+        itx(0,2) = -512*zoom;
+        itx(1,2) = 512*zoom;
         georef_dest.set_transform(itx);
         GeoTransform geotx( clementine_georef[i], georef_dest );
 
         DiskImageView<PixelGray<uint8> > input(clementine_names[i]);
-        ImageViewRef<PixelGray<uint8> > output = crop( transform( input, geotx, ZeroEdgeExtension(), BicubicInterpolation() ), BBox2i(0,0,4000,4000) );
+        ImageViewRef<PixelGray<uint8> > output =
+          crop( transform( input, geotx, ZeroEdgeExtension(),
+                           BicubicInterpolation() ), BBox2i(0,0,1024,1024) );
 
-        //std::map<std::string,std::string> gdal_options;
-        //        gdal_options["COMPRESS"] = "NONE";
-        //DiskImageResourceGDAL rsrc( fs::path(pinhole_name).replace_extension().string()+".clem.jpg", output.format(),
-        //                                    Vector2i(256,256), gdal_options );
-        //write_georeference( rsrc, georef_dest );
-        write_image( fs::path(pinhole_name).replace_extension().string()+".clem.jpg", output, TerminalProgressCallback("test","Writing:"));
+
+        write_georeferenced_image( fs::path(pinhole_name).replace_extension().string() +
+                                   ".clem.tif", output, georef_dest,
+                                   TerminalProgressCallback("test","Writing:"));
 
         break;
       }
+    }
+
+    { // Extracting interest points from the new clementine image
+      std::string clementine_image =
+        fs::path(pinhole_name).replace_extension(".clem.tif").string();
+      std::string apollo_image =
+        fs::path(pinhole_name).replace_extension(".tif").string();
+
+      const float IDEAL_OBALOG_THRESHOLD = .07;
+      InterestPointList ip_clem, ip_apollo;
+      {
+        OBALoGInterestOperator
+          interest_operator(IDEAL_OBALOG_THRESHOLD/10);
+        IntegralInterestPointDetector<OBALoGInterestOperator>
+          detector( interest_operator );
+        DiskImageView<PixelGray<uint8> > clem(clementine_image),
+          apollo(apollo_image);
+        ip_clem = detect_interest_points(clem, detector);
+        ip_apollo = detect_interest_points(apollo, detector);
+        SGradDescriptorGenerator descriptor;
+        descriptor(clem, ip_clem);
+        descriptor(apollo, ip_apollo);
+      }
+
+      { // Matching
+        std::vector<InterestPoint> ip_clem_v, ip_apollo_v;
+        std::copy( ip_clem.begin(), ip_clem.end(),
+                   std::back_inserter(ip_clem_v) );
+        std::copy( ip_apollo.begin(), ip_apollo.end(),
+                   std::back_inserter(ip_apollo_v) );
+        DefaultMatcher matcher(0.7);
+        std::vector<InterestPoint> matched_ip1, matched_ip2;
+        matcher(ip_clem_v, ip_apollo_v, matched_ip1, matched_ip2, false,
+                TerminalProgressCallback( "tools.ipalign", "Matching:"));
+
+        std::vector<Vector3> ransac_ip1 = iplist_to_vectorlist(matched_ip1);
+        std::vector<Vector3> ransac_ip2 = iplist_to_vectorlist(matched_ip2);
+        Matrix<double> align_matrix;
+
+        std::vector<int> indices;
+        math::RandomSampleConsensus<math::HomographyFittingFunctor, math::InterestPointErrorMetric> ransac(math::HomographyFittingFunctor(), math::InterestPointErrorMetric(), 10);
+        align_matrix = ransac(ransac_ip2,ransac_ip1);
+        indices = ransac.inlier_indices(align_matrix,ransac_ip2,ransac_ip1);
+
+        std::vector<InterestPoint> final_ip1, final_ip2;
+        for (unsigned idx=0; idx < indices.size(); ++idx) {
+          final_ip1.push_back(matched_ip1[indices[idx]]);
+          final_ip2.push_back(matched_ip2[indices[idx]]);
+        }
+        match_filename =
+          fs::path(clementine_image).replace_extension().string() + "__" +
+          fs::path(apollo_image).stem() + ".match";
+        write_binary_match_file(match_filename, final_ip1, final_ip2);
+      }
+    }
+
+    { // Looking up pixel locations into GCP positions
+      std::vector<InterestPoint> ip1, ip2; // First one is on the clementine
+      read_binary_match_file(match_filename, ip1, ip2);
+      std::string clementine_image =
+        fs::path(pinhole_name).replace_extension(".clem.tif").string();
+      GeoReference clem_georef, lola_georef;
+      read_georeference( clem_georef, clementine_image );
+      read_georeference( lola_georef, lola_names );
+
+      DiskImageView<double> lola(lola_names);
+      ImageViewRef<double> lola_intrp =
+        interpolate(lola,BicubicInterpolation(),ZeroEdgeExtension());
+
+      ControlNetwork cnet(pinhole_name,ControlNetwork::ImageToGround);
+      for ( unsigned i = 0; i < ip1.size() && i < 10; i++ ) {
+        Vector2 lonlat = clem_georef.pixel_to_lonlat(Vector2(ip1[i].x,ip1[i].y));
+        Vector2 lola_px = lola_georef.lonlat_to_pixel(lonlat);
+        double radius = lola_intrp(lola_px[0],lola_px[1]) +
+          lola_georef.datum().radius( lonlat[0], lonlat[1] );
+
+        ControlPoint cpoint(ControlPoint::GroundControlPoint);
+        cpoint.set_position(LonLatRadToXYZFunctor()(Vector3(lonlat[0],lonlat[1],radius)));
+        cpoint.set_sigma(Vector3(300,300,300));
+        ControlMeasure cm(ip2[i].x*5725./1024.,
+                          ip2[i].y*5725./1024.,
+                          5,5,0);
+        cm.set_serial(fs::path(pinhole_name).replace_extension().string());
+        cpoint.add_measure(cm);
+      }
+      std::string cnet_file =
+        fs::path(pinhole_name).replace_extension("cnet").string();
+      std::cout << "Writing: " << cnet_file << "\n";
+      cnet.write_binary(cnet_file);
     }
   }
 }

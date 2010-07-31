f686727c15eafc9cf00ca97e001b8625e6fde4d5
Zachary Moratto <zachary.m.moratto@nasa.gov>
2010-07-31 11:03:28 -0700

    Update to the current working stereo processing

--- Python/Stereo/stereo_processing.py
+++ Python/Stereo/stereo_processing.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# This is meant for the Harpertown Cores. You should be spawning 4 processes that all think they can have 4 threads. Each process must be limited to 2GB of RAM.
+
 import os, glob, subprocess, sys, time
 from multiprocessing import Pool
 
@@ -14,6 +16,10 @@ if __name__ == '__main__':
         print sys.argv[0] + " requires at least two inputs. First is the working directory. Everything else is the number of nodes avialable for processing. You the user should provide this by `cat $PBS_NODEFILE`\n";
         sys.exit();
 
+    # Discover my job number
+    job_number = int(sys.argv[2])
+    number_of_jobs = int(sys.argv[3])
+
     # Discover my node number
     my_node_number = 0;
     name_proc = subprocess.Popen("hostname",shell=True,stdout=subprocess.PIPE);
@@ -21,9 +27,9 @@ if __name__ == '__main__':
 
     print "My node name is: "+my_node_name;
 
-    for i in range(2,len(sys.argv)):
+    for i in range(4,len(sys.argv)):
         if ( sys.argv[i] == my_node_name ):
-            my_node_number = i-2;
+            my_node_number = i-4;
 
     print "My node number is: "+str(my_node_number);
 
@@ -31,8 +37,8 @@ if __name__ == '__main__':
     time.sleep(1);
 
     task_number = my_node_number;
-    number_of_tasks = len(sys.argv)-2;
-    print "Assigned task "+str(task_number)+"/"+str(number_of_tasks)
+    number_of_tasks = len(sys.argv)-4;
+    print "Assigned task "+str(task_number)+"/"+str(number_of_tasks)+" of Job "+str(job_number)+"/"+str(number_of_jobs)
     name_proc = subprocess.Popen("which stereo",shell=True,stdout=subprocess.PIPE);
     print "My stereo is: "+name_proc.stdout.readline().strip();
 
@@ -47,11 +53,18 @@ if __name__ == '__main__':
     files = glob.glob("*.cub");
     files.sort();
 
-    # Determing task range
-    min_index = int((len(files)-1)*task_number/number_of_tasks);
-    max_index = int((len(files)-1)*(task_number+1)/number_of_tasks);
+    # Determing job's rang
+    jobs_min_index = int((len(files)-1)*float(job_number)/float(number_of_jobs))
+    jobs_max_index  = int((len(files)-1)*float(job_number+1)/float(number_of_jobs))
+    tasks_todo = jobs_max_index - jobs_min_index;
 
-    for i in range(min_index,max_index):
+    # Determing task range
+    min_index = int((tasks_todo)*task_number/number_of_tasks);
+    max_index = int((tasks_todo)*(task_number+1)/number_of_tasks);
+
+    print "My index range: "+str(jobs_min_index+min_index)+" - "+str(jobs_min_index+max_index)
+
+    for i in range(jobs_min_index+min_index,jobs_min_index+max_index):
         startidx = files[i].find("-M-");
         endidx = files[i].find(".");
         diff = int(files[i+1][startidx+3:endidx])-int(files[i][startidx+3:endidx]);
@@ -62,6 +75,7 @@ if __name__ == '__main__':
         cam2 = files[i+1][:files[i+1].find(".")] + ".isis_adjust"
         cam1_alt = files[i][:files[i].rfind(".")] + ".isis_adjust"
         cam2_alt = files[i+1][:files[i+1].rfind(".")] + ".isis_adjust"
+
         id1 = (files[i].split("-M-")[1]).split(".")[0]
         id2 = (files[i+1].split("-M-")[1]).split('.')[0]
         output_dir = id1 + "_" + id2 + "-stereo"
@@ -94,21 +108,21 @@ if __name__ == '__main__':
         if (os.path.exists(full_prefix+"-DEM.tif")):
             print "Found finished dem - skipping"
         else:
-            point2dem_cmd = "point2dem " + full_prefix + "-PC.tif --xyz -r moon --default-value -32768 --cache-dir .";
+            point2dem_cmd = "point2dem " + full_prefix + "-PC.tif --xyz -r moon --default-value -32767 --cache-dir .";
             point2dem_cmds.append(point2dem_cmd);
 
         # Building hillshade command
         if (os.path.exists(full_prefix+"-DEM_HILLSHADE.tif")):
             print "Found finished hillshade - skipping"
         else:
-            hillshade_cmd = "hillshade " + full_prefix + "-DEM.tif --nodata-value -32768";
+            hillshade_cmd = "hillshade " + full_prefix + "-DEM.tif --nodata-value -32767";
             hillshade_cmds.append(hillshade_cmd);
 
         # Building colormap command
         if (os.path.exists(full_prefix+"-DEM_CMAP.tif")):
             print "Found finished colormap - skipping"
         else:
-            colormap_cmd = "colormap " + full_prefix + "-DEM.tif --nodata-value -32768 -s " + full_prefix + "-DEM_HILLSHADE.tif --min -8499 --max 10208"
+            colormap_cmd = "colormap " + full_prefix + "-DEM.tif --nodata-value -32767 -s " + full_prefix + "-DEM_HILLSHADE.tif --min -8499 --max 10208"
             colormap_cmds.append(colormap_cmd);
 
         # Building orthoproject command
@@ -129,7 +143,7 @@ if __name__ == '__main__':
                 orthoproject_cmds.append( orthoproject_cmd );
 
     # Creating work pool and processing
-    pool = Pool(processes=3)
+    pool = Pool(processes=8)
 
     print "--- Node "+str(my_node_number)+" processing STEREO ---\n";
     results = [pool.apply_async(job_func, (cmd,)) for cmd in stereo_cmds]

f6e5d1676725b71e9fc2ffa658da37b576e1f6be
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-07-31 17:43:29 -0700

    Added 3rd party library ANN

--- tools/src/apollo_match.cc
+++ tools/src/apollo_match.cc
@@ -11,6 +11,8 @@
 #include <vw/Mosaic/ImageComposite.h>
 #include "ransac.h"
 
+#include <ANN/ANN.h>
+
 using namespace vw;
 using namespace vw::ip;
 
@@ -19,6 +21,88 @@ namespace po = boost::program_options;
 
 #include <boost/filesystem/path.hpp>
 namespace fs = boost::filesystem;
+
+class InterestPointMatcherANN {
+  double m_threshold;
+
+public:
+
+  InterestPointMatcherANN( double threshold = 0.5 ) : m_threshold(threshold) {}
+
+  /// Given two lists of interest points, this routine returns the two
+  /// losts of matching interest points.
+  template <class ListT, class MatchListT>
+  void operator()( ListT const& ip1, ListT const& ip2,
+                   MatchListT& matched_ip1, MatchListT& matched_ip2,
+                   bool /*bidirectional*/ = false,
+                   const ProgressCallback &progress_callback = ProgressCallback::dummy_instance() ) const {
+    typedef typename ListT::const_iterator IterT;
+
+    matched_ip1.clear(); matched_ip2.clear();
+    if (!ip1.size() || !ip2.size()) {
+      vw_out(InfoMessage,"interest_point") << "No points to match, exiting\n";
+      progress_callback.report_finished();
+      return;
+    }
+
+    int number_of_positive = 0;
+    BOOST_FOREACH( InterestPoint const& ip, ip1 ) {
+      if ( ip.polarity )
+        number_of_positive++;
+    }
+    std::cout << "Positive " << number_of_positive << "/" << ip1.size() << "\n";
+
+    // Allocating negative and positive ANN
+    double eps = 0.0;
+    ANNpointArray ann_pts;
+    ann_pts = annAllocPts( ip2.size(), ip2.begin()->size() );
+    int count = 0;
+    for ( IterT ip = ip2.begin(); ip != ip2.end(); ip++ ) {
+      std::copy( ip->begin(), ip->end(), ann_pts[count] );
+      count++;
+    }
+    ANNkd_tree* kdtree = new ANNkd_tree( ann_pts, ip2.size(),
+                                         ip2.begin()->size() );
+
+    // Making Searches in ANN
+    progress_callback.report_progress(0);
+    std::vector<int> match_index( ip1.size() );
+    Vector2 nn_distances;
+    Vector2i nn_indexes;
+    Vector<float> query( ip1.begin()->size() );
+    float inc_amt = 1/float(ip1.size());
+    count = 0;
+    BOOST_FOREACH( InterestPoint const& ip, ip1 ) {
+      if (progress_callback.abort_requested())
+        vw_throw( Aborted() << "Aborted by ProgressCallback" );
+      progress_callback.report_incremental_progress(inc_amt);
+
+      std::copy( ip.begin(), ip.end(), query.begin() );
+      kdtree->annkSearch( &query[0], 2,
+                          &nn_indexes[0], &nn_distances[0], eps );
+
+      if ( nn_distances[0] > m_threshold * nn_distances[1] )
+        match_index[count] = -1;
+      else
+        match_index[count] = nn_indexes[0];
+      count++;
+    }
+    progress_callback.report_finished();
+
+    // Deallocating ANN
+    delete kdtree;
+    annDeallocPts( ann_pts );
+    annClose();
+
+    // Building matched_ip1 & matched ip 2
+    for (unsigned i = 0; i < ip1.size(); i++ ) {
+      if ( match_index[i] != -1 ) {
+        matched_ip1.push_back( ip1[i] );
+        matched_ip2.push_back( ip2[match_index[i]] );
+      }
+    }
+  }
+};
 
 // Duplicate matches for any given interest point probably indicate a
 // poor match, so we cull those out here.
@@ -104,7 +188,8 @@ int main(int argc, char** argv) {
       std::vector<InterestPoint> matched_ip1, matched_ip2;
 
       // Run interest point matcher that does not use KDTree algorithm.
-      InterestPointMatcherSimple<L2NormMetric,NullConstraint> matcher(matcher_threshold);
+      //InterestPointMatcherSimple<L2NormMetric,NullConstraint> matcher(matcher_threshold);
+      InterestPointMatcherANN matcher( matcher_threshold );
       matcher(ip1, ip2, matched_ip1, matched_ip2, false,
               TerminalProgressCallback( "tools.ipmatch","Matching:"));
 
